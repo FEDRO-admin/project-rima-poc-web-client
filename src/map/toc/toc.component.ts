be9b5fc98b2838ad40b0392e1aa3c347164725f5
Adam Mihočka <adam.mihocka@ebp.ch>
2026-05-04 16:10:46 +0200

    Add a blank pane as a placeholder for the layer tree

---
+++ src/map/toc/toc.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rima-toc',
+  imports: [],
+  templateUrl: './toc.component.html',
+  styleUrl: './toc.component.scss',
+})
+export class TocComponent {}
