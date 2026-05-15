57851f7f183ed1409183ee29664f1aa62fe14a9a
Manuel Baer <manuel.baer@ebp.ch>
2026-05-15 10:36:37 +0200

    Update the mapserver and featureserver URLs to point to the newly published map.

--- src/map/layers/map-server.config.ts
+++ src/map/layers/map-server.config.ts
@@ -1,5 +1,5 @@
 export const MAP_SERVER_URL =
-  'https://rima-poc.switzerlandnorth.cloudapp.azure.com/arcgis/rest/services/RIMA_test_05_14_14_47_MIL1/MapServer';
+  'https://rima-poc.switzerlandnorth.cloudapp.azure.com/arcgis/rest/services/RIMA_Map_MIL1/MapServer';
 
 export const FEATURE_SERVER_URL =
-  'https://rima-poc.switzerlandnorth.cloudapp.azure.com/arcgis/rest/services/RIMA_test_05_14_14_47_MIL1/FeatureServer';
+  'https://rima-poc.switzerlandnorth.cloudapp.azure.com/arcgis/rest/services/RIMA_Map_MIL1/FeatureServer';
