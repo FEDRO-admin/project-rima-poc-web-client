bf6b785cc54f0f7764cf4b86f5e04b2fc0651a1b
Manuel Baer <manuel.baer@ebp.ch>
2026-09-02 08:37:31 +0200

    Integrating ESRI components into langauge switching.

--- src/i18n/language-effects.ts
+++ src/i18n/language-effects.ts
@@ -1,5 +1,7 @@
+import { DOCUMENT } from '@angular/common';
 import { effect, inject, Injectable, untracked } from '@angular/core';
 import { TranslocoService } from '@jsverse/transloco';
+import { setLocale } from '@arcgis/core/intl';
 import { LanguageStore } from './language.store';
 @Injectable({
   providedIn: 'root',
@@ -7,12 +9,17 @@ import { LanguageStore } from './language.store';
 export class LanguageEffect {
   private readonly translocoService = inject(TranslocoService);
   private readonly languageStore = inject(LanguageStore);
+  private readonly document = inject(DOCUMENT);
 
   constructor() {
     effect(() => {
       const activeLanguage = this.languageStore.activeLanguage();
       untracked(() => {
         this.translocoService.setActiveLang(activeLanguage);
+        // Syncs ArcGIS/Calcite web components, which read locale from the root `lang` attribute
+        this.document.documentElement.lang = activeLanguage;
+        // Syncs @arcgis/core internals (date/number formatting, popups, basemap place labels)
+        setLocale(activeLanguage);
       });
     });
   }
